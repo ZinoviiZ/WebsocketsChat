d33bda8c98410bd647b01d087daf8e1a8d39f13d
zinoviyzubko <zinoviy.zubko@gmail.com>
2017-04-09 08:59:07 +0300

    Before pull master

---
+++ src/main/java/com/risingapp/likeit/service/WebSocketService.java
@@ -0,0 +1,16 @@
+package com.risingapp.likeit.service;
+
+import com.risingapp.likeit.model.request.SendMessageRequest;
+import com.risingapp.likeit.model.response.SendMessageResponse;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by zinoviyzubko on 09.04.17.
+ */
+@Service
+public class WebSocketService {
+
+    private SendMessageResponse sendMessage(SendMessageRequest request) {
+        return null;
+    }
+}

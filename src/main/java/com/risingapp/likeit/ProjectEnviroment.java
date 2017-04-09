9d6e684ab65e5de9f2d4dc376ea335bbb78cf367
oleg <lolego1601@gmail.com>
2017-04-09 09:03:37 +0300

    minor fixes

--- src/main/java/com/risingapp/likeit/ProjectEnviroment.java
+++ src/main/java/com/risingapp/likeit/ProjectEnviroment.java
@@ -5,6 +5,6 @@ package com.risingapp.likeit;
  */
 public class ProjectEnviroment {
 
-    public static final String ENVIRONMENT_URL = "https://likeit-risingapp.herokuapp.com/rest/photos/";
-//    public static final String ENVIRONMENT_URL = "http://172.20.10.3:8080/rest/photos/";
+//    public static final String ENVIRONMENT_URL = "https://likeit-risingapp.herokuapp.com/rest/photos/";
+    public static final String ENVIRONMENT_URL = "http://172.20.10.3:8080/rest/photos/";
 }

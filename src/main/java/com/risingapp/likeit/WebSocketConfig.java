0b99391ff1d004423ac27720c82cdea45c2adbfe
oleg <lolego1601@gmail.com>
2017-04-09 09:54:52 +0300

    empty

--- src/main/java/com/risingapp/likeit/WebSocketConfig.java
+++ src/main/java/com/risingapp/likeit/WebSocketConfig.java
@@ -17,6 +17,7 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-api").setAllowedOrigins("*");
+
     }
 
 //    @Override
